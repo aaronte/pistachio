dd0ee576fdc1e71caaf64db43362889123663e24
Aaron Te <aaronjte@gmail.com>
2015-06-07 15:51:24 -0400

    Fixed tab titles not showing.

--- www/appConfig/stateProviderConfig.js
+++ www/appConfig/stateProviderConfig.js
@@ -1,43 +1,45 @@
 angular
     .module('pistachio')
-    .config(function ($stateProvider, $urlRouterProvider) {
+    .config(Config);
 
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
-        $stateProvider
 
-            // setup an abstract state for the tabs directive
-            .state('tab', {
-                url: "/tab",
-                abstract: true,
-                templateUrl: "templates/tabs.html"
-            })
+function Config($stateProvider, $urlRouterProvider) {
 
-            // Each tab has its own nav history stack:
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-            .state('tab.add', {
-                url: '/add',
-                views: {
-                    'tab-add': {
-                        templateUrl: 'templates/tab-add.html',
-                        controller: 'PurchaseController as purchaseCtrl'
-                    }
+        // setup an abstract state for the tabs directive
+        .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+        })
+
+        // Each tab has its own nav history stack:
+
+        .state('tab.add', {
+            url: '/add',
+            views: {
+                'tab-add': {
+                    templateUrl: 'templates/tab-add.html',
+                    controller: 'PurchaseController as purchaseCtrl'
                 }
-            })
+            }
+        })
 
-            .state('tab.dash', {
-                url: '/dash',
-                views: {
-                    'tab-dash': {
-                        templateUrl: 'templates/tab-dash.html',
-                        controller: 'DashController as dashCtrl'
-                    }
+        .state('tab.dash', {
+            url: '/dash',
+            views: {
+                'tab-dash': {
+                    templateUrl: 'templates/tab-dash.html',
+                    controller: 'DashController as dashCtrl'
                 }
-            });
+            }
+        });
 
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/tab/add');
-
-    });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/add');
+}
