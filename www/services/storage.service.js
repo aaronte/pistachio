62cfd921d670ec80f68de4aef8fbc2f6c8645894
Aaron Te <aaronjte@gmail.com>
2015-06-07 17:18:02 -0400

    Adding vendors transaction to SQLite.

--- www/services/storage.service.js
+++ www/services/storage.service.js
@@ -10,23 +10,32 @@ function storage($cordovaSQLite, $q) {
     //var DB = $cordovaSQLite.openDB({ name: DB_NAME });
     var DB = window.openDatabase(DB_NAME, '1.0', 'Cordova Demo', 200000);
     var TRANSACTIONS_SCHEMA = '(vendor_name, amount, date)';
+    var VENDORS_SCHEMA = '(vendor_name, date_added)';
 
     self.transactions = [];
+    self.vendors = [];
 
     self.setUp = setUp;
+
     self.insertTransaction = insertTransaction;
     self.getTransactions = getTransactions;
 
+    self.insertPayee = insertPayee;
+    self.getPayee = getPayee;
+
     function setUp() {
         var query = {
-            dropTable: 'DROP TABLE IF EXISTS transactions',
-            createTable: 'CREATE TABLE IF NOT EXISTS transactions ' + TRANSACTIONS_SCHEMA
+            dropTransactionsTable: 'DROP TABLE IF EXISTS transactions',
+            createTransactionsTable: 'CREATE TABLE IF NOT EXISTS transactions ' + TRANSACTIONS_SCHEMA,
+            dropVendorsTable: 'DROP TABLE IF EXISTS vendors',
+            createVendorsTable: 'CREATE TABLE IF NOT EXISTS vendors' + VENDORS_SCHEMA
         };
 
         // Uncomment if you want to drop your current 'transactions' table
         //$cordovaSQLite.execute(DB, query.dropTable);
 
-        $cordovaSQLite.execute(DB, query.createTable).then(function() {}, logError);
+        $cordovaSQLite.execute(DB, query.createTransactionsTable).then(function() {}, logError);
+        $cordovaSQLite.execute(DB, query.createTransactionsTable).then(function() {}, logError);
     }
 
     function insertTransaction(vendorName, price) {
@@ -43,6 +52,23 @@ function storage($cordovaSQLite, $q) {
 
         $cordovaSQLite.execute(DB, query).then(function(result) {
             self.transactions = angular.copy(result.rows);
+            deferredPromise.resolve(result.rows);
+        });
+
+        return deferredPromise.promise;
+    }
+
+    function insertPayee(payeeName) {
+        var query = 'INSERT INTO vendors ' + VENDORS_SCHEMA + 'VALUES (?, ?)';
+        $cordovaSQLite.execute(DB, query, [payeeName, Date.now()]).then(function() {}, logError);
+    }
+
+    function getPayee() {
+        var deferredPromise = $q.defer();
+        var query = 'SELECT * FROM vendors';
+
+        $cordovaSQLite.execute(DB, query).then(function(result) {
+            self.vendors = angular.copy(result.rows);
             deferredPromise.resolve(result.rows);
         });
 
