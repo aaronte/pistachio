e6f5102259fc3b7a6933bd2ae5f7562100ec3f8d
Aaron Te <aaronjte@gmail.com>
2015-06-07 16:06:51 -0400

    Adding timestamp when inserting transactions.

--- www/services/storage.service.js
+++ www/services/storage.service.js
@@ -9,7 +9,7 @@ function storage($cordovaSQLite, $q) {
     var DB_NAME = 'pistachio.db';
     //var DB = $cordovaSQLite.openDB({ name: DB_NAME });
     var DB = window.openDatabase(DB_NAME, '1.0', 'Cordova Demo', 200000);
-    var TRANSACTIONS_SCHEMA = '(vendor_name, amount)';
+    var TRANSACTIONS_SCHEMA = '(vendor_name, amount, date)';
 
     self.transactions = [];
 
@@ -23,6 +23,9 @@ function storage($cordovaSQLite, $q) {
             createTable: 'CREATE TABLE IF NOT EXISTS transactions ' + TRANSACTIONS_SCHEMA
         };
 
+        // Uncomment if you want to drop your current 'transactions' table
+        //$cordovaSQLite.execute(DB, query.dropTable);
+
         $cordovaSQLite.execute(DB, query.createTable).then(createSuccess, logError);
 
         function createSuccess(result) {
@@ -31,9 +34,9 @@ function storage($cordovaSQLite, $q) {
     }
 
     function insertTransaction(vendorName, price) {
-        var query = 'INSERT INTO transactions ' + TRANSACTIONS_SCHEMA + ' VALUES (?, ?)';
+        var query = 'INSERT INTO transactions ' + TRANSACTIONS_SCHEMA + ' VALUES (?, ?, ?)';
 
-        $cordovaSQLite.execute(DB, query, [vendorName, price]).then(function(result) {
+        $cordovaSQLite.execute(DB, query, [vendorName, price, Date.now()]).then(function(result) {
             console.log(result);
         }, logError);
     }
